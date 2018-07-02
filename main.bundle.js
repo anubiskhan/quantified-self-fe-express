57823ad1bd7e13a97ea9e8a83c9f0b8d3e01bd90
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-02 16:01:22 -0600

    Fixes typo in URL and runs build

--- main.bundle.js
+++ main.bundle.js
@@ -414,7 +414,7 @@
 	  if (host === "localhost" || host === "127.0.0.1") {
 	    return "http://localhost:3000";
 	  } else {
-	    return "https://radiant-scrubland-30997.herokuapp.com/api/v1/foods";
+	    return "https://radiant-scrubland-30997.herokuapp.com";
 	  }
 	};
 
